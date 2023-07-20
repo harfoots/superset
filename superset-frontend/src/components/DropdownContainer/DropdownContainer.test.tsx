496a46229709757a13d408addb994f7fad302b01
Thomas Uster <thomasuster@gmail.com>
2023-07-19 18:53:48 -0700

    Update superset-frontend/src/components/DropdownContainer/DropdownContainer.test.tsx:

--- superset-frontend/src/components/DropdownContainer/DropdownContainer.test.tsx
+++ superset-frontend/src/components/DropdownContainer/DropdownContainer.test.tsx
@@ -1,24 +1,8 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+//after
 import React from 'react';
 import userEvent from '@testing-library/user-event';
-import { screen, render } from 'spec/helpers/testing-library';
+import { screen } from 'spec/helpers/testing-library';
+import { createRoot } from 'react-dom/client';
 import Button from '../Button';
 import Icons from '../Icons';
 import DropdownContainer from '.';
@@ -42,27 +26,35 @@ const mockOverflowingIndex = async (
 };
 
 test('renders children', () => {
-  render(<DropdownContainer items={generateItems(3)} />);
+  const container = document.getElementById('app');
+  const root = createRoot(container);
+  root.render(<DropdownContainer items={generateItems(3)} />);
   expect(screen.getByText('Element 1')).toBeInTheDocument();
   expect(screen.getByText('Element 2')).toBeInTheDocument();
   expect(screen.getByText('Element 3')).toBeInTheDocument();
 });
 
 test('renders children with custom horizontal spacing', () => {
-  render(<DropdownContainer items={ITEMS} style={{ gap: 20 }} />);
+  const container = document.getElementById('app');
+  const root = createRoot(container);
+  root.render(<DropdownContainer items={ITEMS} style={{ gap: 20 }} />);
   expect(screen.getByTestId('container')).toHaveStyle('gap: 20px');
 });
 
 test('renders a dropdown trigger when overflowing', async () => {
   await mockOverflowingIndex(3, () => {
-    render(<DropdownContainer items={ITEMS} />);
+    const container = document.getElementById('app');
+    const root = createRoot(container);
+    root.render(<DropdownContainer items={ITEMS} />);
     expect(screen.getByText('More')).toBeInTheDocument();
   });
 });
 
 test('renders a dropdown trigger with custom icon', async () => {
   await mockOverflowingIndex(3, async () => {
-    render(
+    const container = document.getElementById('app');
+    const root = createRoot(container);
+    root.render(
       <DropdownContainer items={ITEMS} dropdownTriggerIcon={<Icons.Link />} />,
     );
     expect(
@@ -74,7 +66,9 @@ test('renders a dropdown trigger with custom icon', async () => {
 test('renders a dropdown trigger with custom text', async () => {
   await mockOverflowingIndex(3, () => {
     const customText = 'Custom text';
-    render(
+    const container = document.getElementById('app');
+    const root = createRoot(container);
+    root.render(
       <DropdownContainer items={ITEMS} dropdownTriggerText={customText} />,
     );
     expect(screen.getByText(customText)).toBeInTheDocument();
@@ -84,7 +78,9 @@ test('renders a dropdown trigger with custom text', async () => {
 test('renders a dropdown trigger with custom count', async () => {
   await mockOverflowingIndex(3, () => {
     const customCount = 99;
-    render(
+    const container = document.getElementById('app');
+    const root = createRoot(container);
+    root.render(
       <DropdownContainer items={ITEMS} dropdownTriggerCount={customCount} />,
     );
     expect(screen.getByTitle(customCount)).toBeInTheDocument();
@@ -92,13 +88,17 @@ test('renders a dropdown trigger with custom count', async () => {
 });
 
 test('does not render a dropdown button when not overflowing', () => {
-  render(<DropdownContainer items={generateItems(3)} />);
+  const container = document.getElementById('app');
+  const root = createRoot(container);
+  root.render(<DropdownContainer items={generateItems(3)} />);
   expect(screen.queryByText('More')).not.toBeInTheDocument();
 });
 
 test('renders a dropdown when overflowing', async () => {
   await mockOverflowingIndex(3, () => {
-    render(<DropdownContainer items={ITEMS} />);
+    const container = document.getElementById('app');
+    const root = createRoot(container);
+    root.render(<DropdownContainer items={ITEMS} />);
     userEvent.click(screen.getByText('More'));
     expect(screen.getByTestId('dropdown-content')).toBeInTheDocument();
   });
@@ -106,7 +106,9 @@ test('renders a dropdown when overflowing', async () => {
 
 test('renders children with custom vertical spacing', async () => {
   await mockOverflowingIndex(3, () => {
-    render(<DropdownContainer items={ITEMS} dropdownStyle={{ gap: 20 }} />);
+    const container = document.getElementById('app');
+    const root = createRoot(container);
+    root.render(<DropdownContainer items={ITEMS} dropdownStyle={{ gap: 20 }} />);
     userEvent.click(screen.getByText('More'));
     expect(screen.getByTestId('dropdown-content')).toHaveStyle('gap: 20px');
   });
@@ -115,7 +117,9 @@ test('renders children with custom vertical spacing', async () => {
 test('fires event when overflowing state changes', async () => {
   await mockOverflowingIndex(3, () => {
     const onOverflowingStateChange = jest.fn();
-    render(
+    const container = document.getElementById('app');
+    const root = createRoot(container);
+    root.render(
       <DropdownContainer
         items={generateItems(5)}
         onOverflowingStateChange={onOverflowingStateChange}
@@ -131,7 +135,9 @@ test('fires event when overflowing state changes', async () => {
 test('renders a dropdown with custom content', async () => {
   await mockOverflowingIndex(3, () => {
     const customDropdownContent = <div>Custom content</div>;
-    render(
+    const container = document.getElementById('app');
+    const root = createRoot(container);
+    root.render(
       <DropdownContainer
         items={ITEMS}
         dropdownContent={() => customDropdownContent}
@@ -144,7 +150,9 @@ test('renders a dropdown with custom content', async () => {
 
 test('Shows tooltip on dropdown trigger hover', async () => {
   await mockOverflowingIndex(3, async () => {
-    render(
+    const container = document.getElementById('app');
+    const root = createRoot(container);
+    root.render(
       <DropdownContainer
         items={generateItems(5)}
         dropdownTriggerTooltip="Test tooltip"
